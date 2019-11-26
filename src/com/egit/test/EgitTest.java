37f3b2b07c2580185143e5a1331a763b55fd8316
LuCccchen <1144298060@qq.comgit config --global user.email 1144298060@qq.com>
2019-11-26 16:46:13 +0800

    lc update

--- src/com/egit/test/EgitTest.java
+++ src/com/egit/test/EgitTest.java
@@ -4,10 +4,8 @@ public class EgitTest {
 	public static void main(String[] args) {
 		System.out.println("aaa");
 		System.out.println("bbb");
-<<<<<<< HEAD
 		System.out.println("ddd");
-=======
 		System.out.println("ccc");
->>>>>>> branch 'master' of https://github.com/LuCccchen/EgitTest.git
+		System.out.println("ckya");
 	}
 }
